bba7c3fae6c02a97b7fb1c8bcfa7f646d5f3d17a
Maria V Ortiz <mariatua2@gmail>
2018-11-16 21:52:12 -0400

    finishin ordenar matrices

--- Parcial 2/Guia de Ejercicios Parcial/ordenarmatriz.pas
+++ Parcial 2/Guia de Ejercicios Parcial/ordenarmatriz.pas
@@ -114,7 +114,7 @@ Procedure OrdAscColum(var A:matriz;columnas,filas:integer);
 var
    f,c,min,posf,posc:integer;
 begin
-     writeln('Ordenar Ascendente por Columnas usando barajas');
+     writeln('Ordenar Ascendente por Columnas usando seleccion directa');
      writeln('');
 
      posc:=1;
@@ -148,7 +148,185 @@ end;
 
 
 
+Beginend;
+
+Function Minimo(A:matriz;inif,inic,columnas,filas:integer;var posf,posc:integer):integer;
+var
+   i,j,min:integer;
+begin
+     min:= A[inif,inic];
+
+    writeln('inic =>', inic);
+    writeln('inif =>', inif);
+    writeln('min inicial=>', min);
+
+
+    //terminar fila comenzada
+
+    for i:=inif to inif do
+    begin
+         for j:=inic to columnas do
+         begin
+            writeln('filas comenzadas');
+            
+            writeln('A[i,j] es A[',i,',',j,'] => ',A[i,j]);
+
+              if ((A[i,j] < min)) then
+                begin
+                     posc:=j;
+                     posf:=i;
+
+                     min:= A[i,j];
+                    
+                     writeln('min en el if=>', min);
+                end;
+         end;
+    end;
+
+     for i:=inif+1 to filas do
+     begin
+         for j:=1 to columnas do
+         begin
+             writeln('filas nuevas');
+            
+            writeln('A[i,j] es A[',i,',',j,'] => ',A[i,j]);
+
+              if ((A[i,j] < min)) then
+                begin
+                     posc:=j;
+                     posf:=i;
+
+                     min:= A[i,j];
+                    
+                     writeln('min en el if=>', min);
+                end;
+         end;
+     end;
+
+    Minimo:=min;
+end;
+
+Procedure OrdAscColum(var A:matriz;columnas,filas:integer);
+var
+   f,c,min,posf,posc:integer;
+begin
+     writeln('Ordenar Ascendente por Columnas usando seleccion directa');
+     writeln('');
+
+     posc:=1;
+     posf:=1;
+
+     for f:=1 to filas do
+     begin
+          for c:=1 to columnas do
+          begin
+               writeln('A[f,c] es A[',f,',',c,'] => ',A[f,c]);
+
+               min:= Minimo (A,f,c,columnas,filas,posf,posc);
+               writeln('min es =>', min);
+
+               A[posf,posc]:= A[f,c];
+               A[f,c]:=min;
+
+            MostrarMatriz(A,columnas,filas);
+          end;
+     end;
+end;
+
+
+Procedure RecorridoEspiral (A:matriz;columnas,filas:integer);
+var
+    comienzofilas,comienzocolumnas,finfilas,fincolumnas,i:integer;
+begin
+    finfilas:=filas;
+    fincolumnas:=columnas;
+    comienzocolumnas:=1;
+    comienzofilas:=1;
+
+    while ((comienzofilas<finfilas) and (comienzocolumnas<fincolumnas)) do
+    begin
+        for i:=1 to fincolumnas do 
+            write(A[comienzofilas,i], ' ');
+        
+        comienzofilas:= comienzofilas + 1;
+
+        for i:=comienzofilas to finfilas do
+            write(A[i,fincolumnas], '  ');
+        
+        fincolumnas:= fincolumnas - 1;
+
+       // $k - starting row index 
+     //   $m - ending row index 
+      //  $l - starting column index 
+       // $n - ending column index 
+       // $i - iterator 
+        
+        if (comienzofilas<finfilas) then
+        begin
+            
+        
+
+            i:= fincolumnas -1;
+            
+            
+            
+            for i:= fincolumnas downto comienzocolumnas do
+            begin
+                write(A[finfilas, i], ' ');
+            end;
+            
+        end;
+
+
+        if (comienzocolumnas<fincolumnas) then
+
+            for i:=fincolumnas 
+
+         /* Print the first column from 
+           the remaining columns */
+        if ($l < $n) 
+        { 
+            for ($i = $m - 1; $i >= $k; --$i) 
+            { 
+                echo $a[$i][$l] . " "; 
+            } 
+            $l++;  
+        }      
+
+
+    end;
+    
+end;
+
+
+
+
 Begin
+     Repeat
+           Write('Introduzca nro. de filas => '); Readln(filas);
+           Write('Introduzca nro. de columnas => '); Readln(columnas);
+
+           if((columnas>30) or (filas>30)) then
+              writeln('El numero de filas/columnas debe ser menor a 30');
+
+     until ((columnas<=30) and (filas<=30));
+
+     LlenarMatriz(A,columnas,filas);
+     writeln('La matriz creada es:');
+     MostrarMatriz(A,columnas,filas);
+    
+    // MostrarAlReves(A,columnas,filas);
+     //OrdAscColum(A,columnas,filas);
+    // writeln('La matriz ordenada ascendentemente es:');
+     //MostrarMatriz(A,columnas, filas);
+
+     RecorridoEspiral(A,columnas,filas);
+     writeln('El recorrido en espiral es:');
+     MostrarMatriz(A,columnas, filas);
+     readln;
+
+
+End.
      Repeat
            Write('Introduzca nro. de filas => '); Readln(filas);
            Write('Introduzca nro. de columnas => '); Readln(columnas);
